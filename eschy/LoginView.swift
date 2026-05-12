fc44bc109d9fcd5c45383a2d37b4ece34596f6cf
mara-ar <abhinavmara02@gmail.com>
2026-05-12 11:04:05 +0530

    created final front end design for sign up view and made a view minor changes to login page

--- eschy/LoginView.swift
+++ eschy/LoginView.swift
@@ -19,170 +19,168 @@ struct LoginView: View {
                 .offset(y: -300)
                 .blur(radius: 166)
             VStack (spacing: 25) {
-                VStack (spacing: 25) {
-                    Text("eschy")
-                        .font(.outfit(size: 48))
+                Text("eschy")
+                    .font(.outfit(size: 48))
+                    .foregroundStyle(.primaryGreen)
+                    .fontWeight(.semibold)
+                    .padding(.top, 100)
+                    .padding(.bottom, 50)
+                VStack (spacing: 10) {
+                    Text("Your personal why in your pocket")
+                        .font(.outfit(size: 24))
+                        .fontWeight(.medium)
                         .foregroundStyle(.primaryGreen)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("Sign in to get started on your addiction free journey")
+                        .font(.outfit(size: 14))
+                        .foregroundStyle(.gray2)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                VStack (spacing: 10) {
+                    TextField("", text: $email)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .padding(.top, 20)
+                        .padding(.leading, 12)
+                        .padding(.bottom, 5)
+                        .font(.outfit(size: 14))
+                        .frame(height: 45)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, lineWidth: 1)
+                        }
+                        .overlay {
+                            Text("Email")
+                                .font(.outfit(size: 12))
+                                .foregroundStyle(.gray)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(.leading, 12)
+                                .padding(.top, 5)
+                        }
+                    
+                    TextField("", text: $password)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .padding(.top, 20)
+                        .padding(.leading, 12)
+                        .padding(.bottom, 5)
+                        .font(.outfit(size: 14))
+                        .frame(height: 45)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, lineWidth: 1)
+                        }
+                        .overlay {
+                            Text("Password")
+                                .font(.outfit(size: 12))
+                                .foregroundStyle(.gray)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(.leading, 12)
+                                .padding(.top, 5)
+                        }
+                }
+                
+                Button {
+                    // TODO: vanilla sign in
+                    print("sign in button")
+                } label: {
+                    Text("Login")
+                        .font(.outfit(size: 14))
                         .fontWeight(.semibold)
-                        .padding(.top, 100)
-                        .padding(.bottom, 50)
-                    VStack (spacing: 10) {
-                        Text("Your personal why right in your pocket")
-                            .font(.outfit(size: 24))
-                            .foregroundStyle(.primaryGreen)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Text("Sign in to get started on your addiction free journey")
-                            .font(.outfit(size: 14))
-                            .foregroundStyle(.gray2)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    VStack (spacing: 10) {
-                        TextField("", text: $email)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                            .padding(.top, 20)
-                            .padding(.leading, 12)
-                            .padding(.bottom, 5)
-                            .font(.outfit(size: 14))
-                            .frame(height: 45)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(.gray3, lineWidth: 1)
-                            }
-                            .overlay {
-                                Text("Email")
-                                    .font(.outfit(size: 12))
-                                    .foregroundStyle(.gray)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                                    .padding(.leading, 12)
-                                    .padding(.top, 5)
-                            }
-                        
-                        TextField("", text: $password)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                            .padding(.top, 20)
-                            .padding(.leading, 12)
-                            .padding(.bottom, 5)
-                            .font(.outfit(size: 14))
-                            .frame(height: 45)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(.gray3, lineWidth: 1)
-                            }
-                            .overlay {
-                                Text("Password")
-                                    .font(.outfit(size: 12))
-                                    .foregroundStyle(.gray)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                                    .padding(.leading, 12)
-                                    .padding(.top, 5)
-                            }
-                    }
-                    
-                    Button {
-                        // TODO: vanilla sign in
-                        print("sign in button")
-                    } label: {
-                        Text("Login")
-                            .font(.outfit(size: 14))
-                            .fontWeight(.semibold)
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 19)
-                            .background(
-                                Capsule()
-                                    .fill(.primaryGreen)
-                            )
-                            .foregroundStyle(.white)
-                            .contentShape(Capsule())
-                    }
-                    
-                    HStack {
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundStyle(.gray2)
-                        Text("or continue with")
-                            .font(.outfit(size: 14))
-                            .foregroundStyle(.gray2)
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundStyle(.gray2)
-                    }
-                    
-                    HStack {
-                        Spacer()
-                        Button {
-                            // TODO: google sign in
-                            print("signing in with google")
-                        } label: {
-                            Image("google-logo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 25)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .fill(.clear)
-                                        .stroke(.gray3, lineWidth: 1)
-                                        .frame(width: 50, height: 50)
-                                )
-                        }
-                        Spacer()
-                        Button {
-                            // TODO: apple sign in
-                            print("sign in with apple")
-                        } label: {
-                            Image("apple-logo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 25)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .fill(.clear)
-                                        .stroke(.gray3, lineWidth: 1)
-                                        .frame(width: 50, height: 50)
-                                )
-                        }
-                        Spacer()
-                        Button {
-                            // TODO: x sign in
-                            print("sign in with x")
-                        } label: {
-                            Image("x-logo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 25)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .fill(.clear)
-                                        .stroke(.gray3, lineWidth: 1)
-                                        .frame(width: 50, height: 50)
-                                )
-                        }
-                        Spacer()
-                    }
-                    
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 19)
+                        .background(
+                            Capsule()
+                                .fill(.primaryGreen)
+                        )
+                        .foregroundStyle(.white)
+                        .contentShape(Capsule())
+                }
+                
+                HStack {
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundStyle(.gray2)
+                    Text("or continue with")
+                        .font(.outfit(size: 14))
+                        .foregroundStyle(.gray2)
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundStyle(.gray2)
+                }
+                
+                HStack {
                     Spacer()
-                    
-                    HStack (spacing: 5) {
-                        Text("Don't have an account?")
+                    Button {
+                        // TODO: google sign in
+                        print("signing in with google")
+                    } label: {
+                        Image("google-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25)
+                            .background(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(.clear)
+                                    .stroke(.gray3, lineWidth: 1)
+                                    .frame(width: 50, height: 50)
+                            )
+                    }
+                    Spacer()
+                    Button {
+                        // TODO: apple sign in
+                        print("sign in with apple")
+                    } label: {
+                        Image("apple-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25)
+                            .background(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(.clear)
+                                    .stroke(.gray3, lineWidth: 1)
+                                    .frame(width: 50, height: 50)
+                            )
+                    }
+                    Spacer()
+                    Button {
+                        // TODO: x sign in
+                        print("sign in with x")
+                    } label: {
+                        Image("x-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25)
+                            .background(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(.clear)
+                                    .stroke(.gray3, lineWidth: 1)
+                                    .frame(width: 50, height: 50)
+                            )
+                    }
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                HStack (spacing: 5) {
+                    Text("Don't have an account?")
+                        .font(.outfit(size: 10))
+                        .fontWeight(.light)
+                        .foregroundStyle(.gray1)
+                    Button {
+                        // TODO: implement going to sign in page
+                        print("go to sign up page")
+                    } label: {
+                        Text("Sign up here")
                             .font(.outfit(size: 10))
-                            .fontWeight(.light)
-                            .foregroundStyle(.gray1)
-                        Button {
-                            // TODO: implement going to sign in page
-                            print("go to sign up page")
-                        } label: {
-                            Text("Sign up here")
-                                .font(.outfit(size: 10))
-                                .fontWeight(.semibold)
-                                .foregroundStyle(.primaryGreen)
-                        }
-                        
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.primaryGreen)
                     }
                     
                 }
-                .padding(.horizontal, 25)
             }
+            .padding(.horizontal, 25)
         }
         
     }
--- eschy/RegisterView.swift
+++ eschy/RegisterView.swift
@@ -8,8 +8,130 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var password1: String = ""
+    @State private var password2: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .fill(.gradient)
+                .frame(height: 500)
+                .offset(y: -300)
+                .blur(radius: 166)
+            VStack (spacing: 25) {
+                VStack (spacing: 10) {
+                    Text("Start your habit-breaking journey right now, right here")
+                        .font(.outfit(size: 24))
+                        .fontWeight(.medium)
+                        .foregroundStyle(.primaryGreen)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("Sign up to start breaking your bad habits one by one")
+                        .font(.outfit(size: 14))
+                        .foregroundStyle(.gray2)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                VStack {
+                    TextField("", text: $username)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .padding(.top, 20)
+                        .padding(.leading, 12)
+                        .padding(.bottom, 5)
+                        .font(.outfit(size: 14))
+                        .frame(height: 45)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, lineWidth: 1)
+                        }
+                        .overlay {
+                            Text("Username")
+                                .font(.outfit(size: 12))
+                                .foregroundStyle(.gray)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(.leading, 12)
+                                .padding(.top, 5)
+                        }
+                    TextField("", text: $email)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .padding(.top, 20)
+                        .padding(.leading, 12)
+                        .padding(.bottom, 5)
+                        .font(.outfit(size: 14))
+                        .frame(height: 45)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, lineWidth: 1)
+                        }
+                        .overlay {
+                            Text("Email")
+                                .font(.outfit(size: 12))
+                                .foregroundStyle(.gray)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(.leading, 12)
+                                .padding(.top, 5)
+                        }
+                    TextField("", text: $password1)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .padding(.top, 20)
+                        .padding(.leading, 12)
+                        .padding(.bottom, 5)
+                        .font(.outfit(size: 14))
+                        .frame(height: 45)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, lineWidth: 1)
+                        }
+                        .overlay {
+                            Text("Password")
+                                .font(.outfit(size: 12))
+                                .foregroundStyle(.gray)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(.leading, 12)
+                                .padding(.top, 5)
+                        }
+                    TextField("", text: $password2)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .padding(.top, 20)
+                        .padding(.leading, 12)
+                        .padding(.bottom, 5)
+                        .font(.outfit(size: 14))
+                        .frame(height: 45)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, lineWidth: 1)
+                        }
+                        .overlay {
+                            Text("Confirm password")
+                                .font(.outfit(size: 12))
+                                .foregroundStyle(.gray)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(.leading, 12)
+                                .padding(.top, 5)
+                        }
+                }
+                Button {
+                    // TODO: create a new user
+                    print("register button")
+                } label: {
+                    Text("Register")
+                        .font(.outfit(size: 14))
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 19)
+                        .background(
+                            Capsule()
+                                .fill(.primaryGreen)
+                        )
+                        .foregroundStyle(.white)
+                        .contentShape(Capsule())
+                }
+            }
+            .padding()
+        }
     }
 }
 
