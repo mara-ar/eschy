d5852e1ef99d53df7821b7edbd67bff342dbb4d9
mara-ar <abhinavmara02@gmail.com>
2026-05-28 12:46:28 +0530

    partially implemented reminder card view (need to use reminder status view)

--- eschy/ReminderCardView.swift
+++ eschy/ReminderCardView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct ReminderCardView: View {
+    let reminder: Reminder
+    
+    var time: String {
+        "\(reminder.hour):\(reminder.minute < 10 ? "0" : "")\(reminder.minute)"
+    }
+    
+    var checkInStatus: CheckInStatus {
+        return .pending
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image("bell-04")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20)
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(.reminderIconBackground)
+                )
+            VStack (alignment: .leading) {
+                Text("\(reminder.label)")
+                    .font(.outfit(size: 14))
+                    .fontWeight(.medium)
+                Text("\(time)")
+                    .font(.outfit(size: 12))
+                    .fontWeight(.regular)
+                    .foregroundStyle(.gray1)
+            }
+            
+            Spacer()
+            
+            CheckInStatusView(status: .pending)
+        }
     }
 }
 
 #Preview {
-    ReminderCardView()
+    let reminder: Reminder = Reminder(id: UUID(), userId: UUID(), habitId: UUID(), hour: 19, minute: 0, label: "Evening reset", active: true)
+    ReminderCardView(reminder: reminder)
 }
