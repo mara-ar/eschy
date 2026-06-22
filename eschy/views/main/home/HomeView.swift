4ace4169a9c9b6bd38e7a6cd45ae3b7d3af87268
mara-ar <abhinavmara02@gmail.com>
2026-06-22 19:26:11 +0530

    better scrolling (whole home view instead of just todays reminders section)

--- eschy/views/main/home/HomeView.swift
+++ eschy/views/main/home/HomeView.swift
@@ -62,44 +62,45 @@ struct HomeView: View {
             // TODO: important information
             ZStack {
                 if viewModel.habits.count > 0 && viewModel.sortedReminderHabitPairs.count > 0 {
-                    VStack (alignment: .leading) {
-                        Text("Active Habits")
-                            .font(.outfit(size: 16))
-                            .fontWeight(.semibold)
-                            .padding(.horizontal, 16)
-                        
-                        ScrollView(.horizontal) {
-                            HStack {
-                                ForEach(viewModel.habits, id: \.id) { habit in
-                                    HabitCardView(habit: habit)
-                                        .containerRelativeFrame(.horizontal, count: 3, spacing: 16)
-                                        .scrollTransition { content, phase in
-                                            content
-                                                .scaleEffect(
-                                                    x: phase.isIdentity ? 1 : 0.8,
-                                                    y: phase.isIdentity ? 1 : 0.8
-                                                )
-                                                .offset(
-                                                    x: phase.value * -10,
-                                                )
-                                        }
-                                }
-                            }
-                            .scrollTargetLayout()
-//                            .padding(1)
-                        }
-                        .safeAreaPadding(.horizontal, 16)
-                        .safeAreaPadding(.vertical, 1)
-                        .contentMargins(0, for: .scrollContent)
-                        .scrollIndicators(.hidden)
-                        .scrollTargetBehavior(.viewAligned)
-                        .frame(maxWidth: .infinity)
-                        
-                        Group {
-                            Text("Today's Reminders")
+                    ScrollView {
+                        VStack (alignment: .leading) {
+                            Text("Active Habits")
                                 .font(.outfit(size: 16))
                                 .fontWeight(.semibold)
-                            ScrollView {
+                                .padding(.horizontal, 16)
+                            
+                            ScrollView(.horizontal) {
+                                HStack {
+                                    ForEach(viewModel.habits, id: \.id) { habit in
+                                        HabitCardView(habit: habit)
+                                            .containerRelativeFrame(.horizontal, count: 3, spacing: 16)
+                                            .scrollTransition { content, phase in
+                                                content
+                                                    .scaleEffect(
+                                                        x: phase.isIdentity ? 1 : 0.8,
+                                                        y: phase.isIdentity ? 1 : 0.8
+                                                    )
+                                                    .offset(
+                                                        x: phase.value * -10,
+                                                    )
+                                            }
+                                    }
+                                }
+                                .scrollTargetLayout()
+                                //                            .padding(1)
+                            }
+                            .safeAreaPadding(.horizontal, 16)
+                            .safeAreaPadding(.vertical, 1)
+                            .contentMargins(0, for: .scrollContent)
+                            .scrollIndicators(.hidden)
+                            .scrollTargetBehavior(.viewAligned)
+                            .frame(maxWidth: .infinity)
+                            
+                            Group {
+                                Text("Today's Reminders")
+                                    .font(.outfit(size: 16))
+                                    .fontWeight(.semibold)
+                                //                            ScrollView {
                                 VStack {
                                     ForEach(viewModel.sortedReminderHabitPairs, id: \.0.id) {r, h in
                                         ReminderCardView(reminder: r, habit: h)
@@ -112,9 +113,10 @@ struct HomeView: View {
                                         .stroke(.gray3, lineWidth: 1)
                                 )
                                 .padding(1)
+                                //                            }
                             }
+                            .padding(.horizontal, 16)
                         }
-                        .padding(.horizontal, 16)
                     }
                 } else {
                     LoadingView(spinnerColor: .primaryGreen)
