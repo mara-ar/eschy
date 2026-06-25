a5a3214fc2cec90d93e8bc0e78ab78723bfd7b32
mara-ar <abhinavmara02@gmail.com>
2026-06-25 17:49:32 +0530

    Completed implementing notification content setup for habit onboarding

--- eschy/views/habitOnboarding/HabitOnboardingNotificationView.swift
+++ eschy/views/habitOnboarding/HabitOnboardingNotificationView.swift
@@ -33,10 +33,15 @@ struct HabitOnboardingNotificationView: View {
                         Text("\(String(habitData.habit))")
                             .font(.outfit(size: 14))
                             .fontWeight(.medium)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                         Text("\(habitData.notificationContent)")
                             .font(.outfit(size: 12))
                             .foregroundStyle(.gray1)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .lineLimit(4)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                     }
+                    .frame(maxWidth: .infinity)
                     
                     Spacer()
                 }
@@ -45,12 +50,45 @@ struct HabitOnboardingNotificationView: View {
                     RoundedRectangle(cornerRadius: 12)
                         .fill(.white)
                 )
-                .padding(.vertical, 20)
-                .padding(.leading, 49)
+                .padding(20)
                 .background(
                     RoundedRectangle(cornerRadius: 12)
                         .fill(.secondaryOrange)
                 )
+                
+                VStack (alignment: .leading) {
+                    Text("Enter Notification Text")
+                        .font(.outfit(size: 12))
+                        .foregroundStyle(.gray1)
+                    TextField("", text: $habitData.notificationContent, axis: .vertical)
+                        .font(.outfit(size: 16))
+                        .fontWeight(.medium)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .lineLimit(10)
+                }
+                .fixedSize(horizontal: false, vertical: true)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(.gray3, lineWidth: 1)
+                )
+                
+                VStack (alignment: .leading, spacing: 5) {
+                    Text("Suggestion")
+                        .font(.outfit(size: 14))
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    HStack (spacing: 5) {
+                        Circle()
+                            .fill(.gray1)
+                            .frame(width: 3, height: 3)
+                        Text("\"Pause. You're in control.\"")
+                            .font(.outfit(size: 12))
+                            .foregroundStyle(.gray1)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .frame(maxWidth: .infinity)
             }
         }
     }
