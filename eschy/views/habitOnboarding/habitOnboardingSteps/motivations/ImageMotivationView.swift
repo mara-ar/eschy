6aaf979d1a98de50b3cb654dd9e660ebcde44538
mara-ar <abhinavmara02@gmail.com>
2026-06-28 17:21:00 +0530

    implemented photos picker and displaying image for image motivation option, still need to implement editing image and previewing image

--- eschy/views/habitOnboarding/habitOnboardingSteps/motivations/ImageMotivationView.swift
+++ eschy/views/habitOnboarding/habitOnboardingSteps/motivations/ImageMotivationView.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ImageMotivationView: View {
     @Binding var sheet: MotivationActiveSheet?
+    @State private var photoPickerIsActive: Bool = false
+    @State private var photoSelection: PhotosPickerItem?
+    @State private var photoImage: Image?
     
     var body: some View {
         NavigationStack {
@@ -50,27 +54,48 @@ struct ImageMotivationView: View {
                 }
                 
                 VStack (spacing: 15) {
-                    Button {
-                        print("upload image")
-                    } label: {
+                    PhotosPicker(
+                        selection: $photoSelection,
+                        matching: .images,
+                        preferredItemEncoding: .current,
+                        photoLibrary: .shared()
+                    ) {
                         VStack (spacing: 10) {
-                            Image("upload")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 36, height: 36)
-                            Text("Upload Image")
-                                .font(.outfit(size: 14))
-                                .fontWeight(.semibold)
-                                .foregroundStyle(.black)
+                            if let photoImage {
+                                photoImage
+                                    .resizable()
+                                    .scaledToFit()
+                            } else {
+                                Image("upload")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 36, height: 36)
+                                Text("Upload Image")
+                                    .font(.outfit(size: 14))
+                                    .fontWeight(.semibold)
+                                    .foregroundStyle(.black)
+                            }
                         }
-                        .padding(.vertical, 40)
+                        .padding(.vertical, photoImage != nil ? 10 : 40)
                         .frame(maxWidth: .infinity)
+                        .foregroundStyle(.black)
                         .background(
                             RoundedRectangle(cornerRadius: 12)
                                 .stroke(.gray3, style: .init(lineWidth: 1, dash: [15, 10]))
                         )
                     }
-                    .foregroundStyle(.black)
+                    .onChange(of: photoSelection) {
+                        Task {
+                            if let imageData = try? await photoSelection?.loadTransferable(type: Data.self) {
+                                let uiImage = UIImage(data: imageData)
+                                if let uiImage {
+                                    photoImage = Image(uiImage: uiImage)
+                                }
+                            } else {
+                                print("Failed to load image")
+                            }
+                        }
+                    }
                     
                     Button {
                         print("go to editing mode")
@@ -102,6 +127,50 @@ struct ImageMotivationView: View {
 
             }
             .padding()
+        }
+        .sheet(isPresented: $photoPickerIsActive, onDismiss: {
+            photoPickerIsActive = false
+        }) {
+            VStack {
+                PhotosPicker(
+                    selection: $photoSelection,
+                    matching: .images,
+                    preferredItemEncoding: .current,
+                    photoLibrary: .shared()
+                ) {
+                    VStack {
+                        Text("Select Photos")
+                        if let photo = photoSelection {
+                            Button {
+                                print("\(photo)")
+                            } label: {
+                                Text("show photo info")
+                            }
+                            
+                        }
+                        if let image = photoImage {
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 250)
+                        }
+                    }
+                }
+                .onChange(of: photoSelection) {
+                    Task {
+                        if let imageData = try? await photoSelection?.loadTransferable(type: Data.self) {
+                            let uiImage = UIImage(data: imageData)
+                            if let uiImage {
+                                photoImage = Image(uiImage: uiImage)
+                            }
+                        } else {
+                            print("Failed to load image")
+                        }
+                    }
+                }
+            }
+            .presentationDetents([.medium])
+            .presentationBackground(.white)
         }
     }
 }
