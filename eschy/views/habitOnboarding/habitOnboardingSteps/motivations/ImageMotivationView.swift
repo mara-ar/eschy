cc3f62bb66718fa194e29396eb0d6ec742eaafed
mara-ar <abhinavmara02@gmail.com>
2026-06-28 13:17:52 +0530

    front end for image motivation

--- eschy/views/habitOnboarding/habitOnboardingSteps/motivations/ImageMotivationView.swift
+++ eschy/views/habitOnboarding/habitOnboardingSteps/motivations/ImageMotivationView.swift
@@ -12,34 +12,96 @@ struct ImageMotivationView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Image Motivation View")
-                Spacer()
-            }
-            .toolbar {
-                ToolbarItem (placement: .topBarLeading) {
+            VStack (spacing: 25) {
+                HStack {
                     Button {
                         sheet = nil
                     } label: {
                         Image(systemName: "xmark")
+                            .padding()
+                            .foregroundStyle(.black)
+                            .background(
+                                Circle()
+                                    .fill(.white)
+                                    .shadow(color: .gray3.opacity(0.5), radius: 5, y: 0)
+                            )
                     }
-
-                }
-                
-                ToolbarItem(placement: .principal) {
+                    
+                    Spacer()
+                    
                     Text("Upload a meaningful image")
                         .font(.outfit(size: 16))
                         .fontWeight(.semibold)
-                }
-                
-                ToolbarItem (placement: .topBarTrailing) {
+                    
+                    Spacer()
+                    
                     Button {
-                        print("create image motivation")
+                        print("submit image-based motivation")
                     } label: {
                         Image(systemName: "checkmark")
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(
+                                Circle()
+                                    .fill(.primaryGreen)
+                                    .shadow(color: .primaryGreen.opacity(0.5), radius: 5, y: 0)
+                            )
                     }
                 }
+                
+                VStack (spacing: 15) {
+                    Button {
+                        print("upload image")
+                    } label: {
+                        VStack (spacing: 10) {
+                            Image("upload")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 36, height: 36)
+                            Text("Upload Image")
+                                .font(.outfit(size: 14))
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.black)
+                        }
+                        .padding(.vertical, 40)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray3, style: .init(lineWidth: 1, dash: [15, 10]))
+                        )
+                    }
+                    .foregroundStyle(.black)
+                    
+                    Button {
+                        print("go to editing mode")
+                    } label: {
+                        HStack (spacing: 5) {
+                            Image("edit")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 16, height: 16)
+                                .foregroundStyle(.black)
+                            Text("Edit")
+                                .font(.outfit(size: 14))
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.black)
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("go to preview / editing mode")
+                } label: {
+                    Text("Preview")
+                        .font(.outfit(size: 14))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.primaryGreen)
+                }
+
             }
+            .padding()
         }
     }
 }
